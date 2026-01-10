b8533ec68ab2bf580f49004d0f21ca76730e88f3
João Victor Nascimento <86272888+joaovnsousa@users.noreply.github.com>
2026-01-10 02:03:02 -0300

    feat: arquivos adicionados

---
+++ java/myapi/src/main/java/com/jao/myapi/MyapiApplication.java
@@ -0,0 +1,31 @@
+package com.jao.myapi;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jao.myapi.domain.Usuario;
+import com.jao.myapi.repositories.UsuarioRepository;
+
+@SpringBootApplication
+public class MyapiApplication implements CommandLineRunner{
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(MyapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Usuario u1 = new Usuario(null, "João", "joao", "123");
+		Usuario u2 = new Usuario(null, "Victor", "victor", "234");
+	
+		usuarioRepository.saveAll(Arrays.asList(u1, u2));
+	}
+
+}
